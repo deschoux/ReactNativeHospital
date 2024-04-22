c07a089aeb8e55d138fc99002ad41d13afb8f57a
Daniel <159890430+deschoux@users.noreply.github.com>
2024-04-22 17:43:52 +0200

    first commit

--- fakeData/fakeSetting.js
+++ fakeData/fakeSetting.js
@@ -2,13 +2,15 @@ export const fakeSettings = [
   { id: 1, libelle: "Profil" },
   { id: 2, libelle: "Mes documents" },
   { id: 3, libelle: "Mes consultations" },
-  { id: 4, libelle: "Mes rendez-vous" },
-  { id: 5, libelle: "Mes assurances" },
-  { id: 6, libelle: "Pharmacie de garde" },
-  { id: 7, libelle: "Appelez Ambulance" },
-  { id: 8, libelle: "Numéros Utiles" },
-  { id: 9, libelle: "Mon Livre Santé" },
-  { id: 10, libelle: "Me déconnecter" },
+  { id: 4, libelle: "Mes consultations en Visio" },
+  { id: 5, libelle: "Mes rendez-vous" },
+  { id: 6, libelle: "Une Urgence" },
+  { id: 7, libelle: "Mes assurances" },
+  { id: 8, libelle: "Pharmacie de garde" },
+  { id: 9, libelle: "Appelez Ambulance" },
+  { id: 10, libelle: "Numéros Utiles" },
+  { id: 11, libelle: "Mon Livre Santé" },
+  { id: 12, libelle: "Me déconnecter" },
   // Ajoutez d'autres entrées selon vos données
 ];
 
